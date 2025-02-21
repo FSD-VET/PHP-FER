8faa18474f191aaa7d77ae103f2b8577c00bc6c5
FSD-VET <fsd.vet@gmail.com>
2025-02-20 22:02:49 -0300

    Resposta

--- Lista 1/exer2resposta.php
+++ Lista 1/exer2resposta.php
@@ -7,34 +7,28 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Resposta</h1>
+    <h1>Resposta do Exercício 2</h1>
 
     <?php
 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if ($_SERVER['RESQUEST_METHOD'] == 'POST') {
 
             try {
                 $valor1 = $_POST['valor1'];
                 $valor2 = $_POST['valor2'];
-                $soma = $valor1 + $valor2;
-                echo "O valor da soma é: $soma";
-        
-                $div = $valor1 / $valor2;
-                //resto da divisã0 = %
-        
-                $mult = $valor1 * $valor2;
+                $sub = $valor1 - $valor2;
+                echo "O valor da subtração é: $sub";
 
-            } catch(Exception $e){  //ele armazema o erro - se existir  //$e é o pedacinho de memória que armazena o erro
+            }catch(Exception $e){  //ele armazema o erro - se existir  //$e é o pedacinho de memória que armazena o erro
                 echo $e->getMessage(); //exibe o erro
             }
+        }
 
-           
 
-        } 
 
-       
-    
     ?>
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
